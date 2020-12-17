5f4ff70bf8cb19381b016284bcdf9e212cd4903c
Roman Vavilov <rvavilov@clarus.lan>
2020-12-17 10:23:56 +0300

    initial commit

---
+++ src/components/SlateRte/SlateRteUI.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+const SlateRteUI = props => {
+
+  return (
+    <div>123</div>
+  );
+}
+
+export default SlateRteUI;
---
+++ src/containers/SlateRte/SlateRte.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import SlateRteUI from '../../components/SlateRte/SlateRteUI';
+
+const SlateRte = props => {
+
+  return (
+    <SlateRteUI/>
+  );
+}
+
+export default SlateRte;
